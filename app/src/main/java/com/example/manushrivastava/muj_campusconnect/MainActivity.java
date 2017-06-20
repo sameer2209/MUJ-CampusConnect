6c1295ac9afecf309abddece1c9b14a5324993a5
manik0007 <manikjaiswal007@gmail.com>
2017-06-20 13:20:24 +0530

    added mainactivity

---
+++ app/src/main/java/com/example/manushrivastava/muj_campusconnect/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.manushrivastava.muj_campusconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+        public void loginact(android.view.View v)
+    {
+        Intent i=new Intent(this,LoginActivity.class);
+        startActivity(i);
+    }
+    public void signupact(android.view.View v)
+    {
+        Intent i=new Intent(this,SignupActivity.class);
+        startActivity(i);
+    }
+}
